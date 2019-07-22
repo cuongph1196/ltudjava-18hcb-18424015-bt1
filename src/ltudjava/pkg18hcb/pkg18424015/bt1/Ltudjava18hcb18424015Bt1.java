6c2349e6a6357b7655d0623014fdce4ace87be6d
cuongph1196 <phancuong1196@gmail.com>
2019-07-22 23:35:58 +0700

    read file csv

--- src/ltudjava/pkg18hcb/pkg18424015/bt1/Ltudjava18hcb18424015Bt1.java
+++ src/ltudjava/pkg18hcb/pkg18424015/bt1/Ltudjava18hcb18424015Bt1.java
@@ -5,6 +5,12 @@
  */
 package ltudjava.pkg18hcb.pkg18424015.bt1;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author phanc
@@ -14,8 +20,24 @@ public class Ltudjava18hcb18424015Bt1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+//        System.out.println("Hello World!");
+//        Person a = new Person();
+//        a.Name = "Phan huy cuong";
+//        a.Age = 22;
+//        a.Height = 173;
+//        System.out.println(a.Name);
+
+          BufferedReader br = new BufferedReader(new FileReader("../Data/Person.csv"));
+		String str ;
+		while (true)
+		{
+			str = br.readLine();
+			if (str==null)
+				break;
+			System.out.println(str);	
+		}
     }
     
 }
---
+++ src/ltudjava/pkg18hcb/pkg18424015/bt1/SinhVien.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ltudjava.pkg18hcb.pkg18424015.bt1;
+
+/**
+ *
+ * @author phanc
+ */
+public class SinhVien {
+    int STT;
+    String MSSV;
+    String HoTen;
+    boolean GioiTinh;
+    String CMND;
+    
+    public String GetMSSV(){
+        return MSSV;
+    }
+    public String GetHoTen(){
+        return HoTen;
+    }
+}
